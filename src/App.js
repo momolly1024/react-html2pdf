98839ed720ad9da5e25c6c570878b7e7370cbf6f
momolly1024 <momolly1024@gmail.com>
2022-06-06 09:52:54 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.css'
 import Pdf from 'react-to-pdf'
+import { useReactToPrint } from 'react-to-print'
 
-const ref = React.createRef()
-
+const detailData = [
+    { id: 1, date: '2022/01/01', bed: 5, cost: 30 },
+    { id: 2, date: '2022/02/01', bed: 6, cost: 10 },
+    { id: 3, date: '2022/02/15', bed: 3, cost: 50 },
+    { id: 4, date: '2022/03/01', bed: 7, cost: 30 },
+    { id: 5, date: '2022/04/01', bed: 7, cost: 30 },
+    { id: 6, date: '2022/05/21', bed: 7, cost: 30 },
+]
 const DetailPdf = () => {
+    const ref = React.createRef()
+
+    const handlePrint = useReactToPrint({
+        content: () => ref.current,
+    })
+
     return (
         <div>
             <Pdf targetRef={ref} filename='code-example.pdf'>
                 {({ toPdf }) => <button onClick={toPdf}>Download Pdf</button>}
             </Pdf>
             <div style={{ marginBottom: 10 }}></div>
+            <div>
+                <button onClick={handlePrint}>Print</button>
+            </div>
             <div ref={ref}>
                 <div className='detailContainer'>
                     <div className='title'>
-                        <div className='left'>Cognito Logo</div>
+                        <div className='left'>
+                            <img
+                                src='https://picsum.photos/id/10/120/60'
+                                alt=''
+                            />
+                        </div>
                         <div className='center'>租賃費用明細表</div>
-                        <div className='right'>Cognito Info</div>
+                        <div className='right'>
+                            <img
+                                src='https://picsum.photos/id/20/120/60'
+                                alt=''
+                            />
+                        </div>
                     </div>
                     <div>
                         <p className='textNoMargin'>產品名稱OOXX</p>
@@ -40,21 +66,13 @@ const DetailPdf = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>2022/01/02</td>
-                                        <td>6</td>
-                                        <td>30</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2022/01/02</td>
-                                        <td>6</td>
-                                        <td>30</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2022/01/02</td>
-                                        <td>6</td>
-                                        <td>30</td>
-                                    </tr>
+                                    {detailData.map((r) => (
+                                        <tr key={r.id}>
+                                            <td>{r.date}</td>
+                                            <td>{r.bed}</td>
+                                            <td>{r.cost}</td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                                 <tfoot>
                                     <tr>
